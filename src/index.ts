ef8d9aab75b8498a62790d298211da96c0beb9f1
Sergi Botargues <sergi.botargues@tusmedia.com>
2022-09-12 21:26:50 +0200

    typescript login y todo

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
-import "./lib/db";
-import express from "express";
-import commentRoutes from "./routes/comment";
+import express, { Application } from 'express';
+import { Server } from 'http';
+import connectDb from './config/db';
+import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes';
+import userRoutes from './routes/userRoutes';
+import orderRoutes from './routes/orderRoutes';
+import uploadRoutes from './routes/uploadRoutes';
+import morgan from 'morgan';
+import { errorHandler, notFound } from './middleware/errorMiddleware';
 import cors from 'cors';
+import path from 'path';
+import sanitizedConfig from './config';
 
-const app = express();
-const port = process.env.PORT || 3333;
-
-app.use(cors());
-
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
-
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /comments to view all the comments" });
+dotenv.config({
+  path: path.resolve(__dirname, '/.env'),
 });
 
-app.use("/comments", commentRoutes);
+connectDb();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const app: Application = express();
+
+if (sanitizedConfig.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/uploads', uploadRoutes);
+
+app.use('/uploads', express.static(path.join(process.cwd(), '/uploads')));
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT: number | string = sanitizedConfig.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
