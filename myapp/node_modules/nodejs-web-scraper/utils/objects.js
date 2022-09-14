cbb09008299e24342c8248f249909b8e9b747507
Sergi Botargues <sbotargues@niikiis.com>
2022-09-14 22:35:31 +0200

    r

---
+++ myapp/node_modules/nodejs-web-scraper/utils/objects.js
@@ -0,0 +1,52 @@
+
+/**
+ * To prevent dictionary key-collision, get a number-appended key. Returns a higher order function, to preserve original key.
+ * @param {string} originalKey 
+ */
+function getDictionaryKey(originalKey) {
+    const func = (keyName, dictionary, counter = 1) => {
+
+        if (!dictionary[keyName]) {
+            // console.log('new file name', newFileName)
+            return keyName;
+        }
+
+        counter = counter + 1;
+        let newKeyName = originalKey + counter;
+
+        return func(newKeyName, dictionary, counter);
+
+    }
+
+    return func;
+
+}
+
+
+
+/**
+ * 
+ * @param {Object} originalObject 
+ * @param {Object} secondaryObject 
+ * @return {void}
+ */
+function deepSpread(originalObject,secondaryObject){
+    // debugger
+    if(!originalObject)
+        originalObject = {}
+
+    for(let prop in secondaryObject){
+        if(typeof secondaryObject[prop] === 'object' && !Array.isArray(secondaryObject[prop])){
+            // debugger;
+            deepSpread(originalObject[prop],secondaryObject[prop]); 
+        }else{
+          originalObject[prop] = secondaryObject[prop]  
+        }
+        
+    }
+}
+
+module.exports = {
+    getDictionaryKey,
+    deepSpread
+}
