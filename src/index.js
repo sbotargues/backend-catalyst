1822df5873256e3e6ebc8f7d56eefc55e0d26766
Sergi Botargues <sergi.botargues@tusmedia.com>
2022-09-14 10:07:16 +0200

    cambios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import "./lib/db";
-import express from "express";
-import commentRoutes from "./routes/comment";
-import cors from 'cors';
+require("./lib/db");
+const express = require("express");
+const commentRoutes = require("./routes/comment");
+const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
