c1458325e908a8abeeea51361e245af94ddeda7c
Sergi Botargues <sbotargues@niikiis.com>
2022-10-04 20:02:33 +0200

    features

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
 
 if (!process.env.MONGO_URL) {
   throw new Error("Please add the MONGO_URL environment variable");
@@ -11,10 +14,52 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const database = mongoose.connection;
 
+require("../app/routes/auth.routes")(app);
+require("../app/routes/user.routes")(app);
+
 database.on(
   "error",
   console.error.bind(console, "❌ mongodb connection error"),
 );
-database.once("open", () => console.log("✅ mongodb connected successfully"));
+database.once("open", () => initial(), console.log("✅ mongodb connected successfully"));
+
+const db = require("../app/models");
+const Role = db.role;
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 mongoose.Promise = Promise;
