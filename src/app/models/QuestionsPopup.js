93a1b2c5389348be8e25271c88e036a6a1fbb20f
Sergi Botargues <sergi.botargues@tusmedia.com>
2022-11-04 14:34:33 +0100

    correction

--- src/app/models/QuestionsPopup.js
+++ src/app/models/QuestionsPopup.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const QuestionsPopup = new Schema(
     {
-        questionOne: { type: Number },
+        questionOne: { type: String },
         questionTwo: { type: Number },
         user:[{ type: Schema.Types.ObjectId, ref: 'User' }]
     },
