331b473fa80d8acb722b9af3f3e0331925f312a7
Sergi Botargues <sergi.botargues@tusmedia.com>
2022-11-30 23:08:56 +0100

    refactor

--- src/app/controllers/words.js
+++ src/app/controllers/words.js
@@ -36,7 +36,7 @@ createWord = (req, res) => {
 getWordRandom = async (req, res) => {
     //script poco optimizado
 
-    Word.count().exec(function (err, count) {
+    Word.countDocuments().exec(function (err, count) {
         var random = Math.floor(Math.random() * count);
         Word.findOne()
             .skip(random)
@@ -45,39 +45,6 @@ getWordRandom = async (req, res) => {
       });
   });
 };
-    //script que devuelve el array de ids palabras random
-
-//  await Word.find({}, (err, words) => {
-//    if (err) {
-//      return res.status(400).json({ success: false, error: err });
-//    }
-//    if (!words.length) {
-//      return res
-//        .status(404)
-//        .json({ success: false, error: `Palabra no encontrado` });
-//    }
-//    const data = words.map(({ _id }) => ({ _id }));
-//    var _ids = [];
-//    for (var i = 0; i < data.length; i++) {
-//      _ids.push(data[i]._id);
-//    }
-//    return res.status(200).json({ success: true, data: _ids });
-//  }).catch((err) => console.log(err));
- 
-//         const randomWord = data[Math.floor(Math.random() * data.length)];
-//     await Word.findOne({ _id: randomWord }, (err, word) => {
-//         if (err) {
-//             return res.status(400).json({ success: false, error: err });
-//         }
-
-//         if (!word) {
-//             return res
-//                 .status(404)
-//                 .json({ success: false, error: `palabra no encontrada` });
-//         }
-//         return res.status(200).json({ success: true, data: word });
-//     }).catch((err) => console.log(err));
-// };
 
 getWordById = async (req, res) => {
     await Word.findOne({ _id: req.params.id }, (err, word) => {
